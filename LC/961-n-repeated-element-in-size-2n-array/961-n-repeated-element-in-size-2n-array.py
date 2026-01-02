78d24b98f72870bf3d15bfa99db3dab9ff581ece
rakshapahariya <rakshapahariya1208@gmail.com>
2026-01-02 22:00:00 +0530

    LC DAILY - 691

--- LC/961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.py
+++ LC/961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.py
@@ -2,4 +2,4 @@ class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         return int((sum(nums)-sum(set(nums)))//((len(nums)/2)-1))
     
-    #didn't had time to brainstrom it -- so going brute!
+    #didn't had time to brainstrom it -- so going brute!!
